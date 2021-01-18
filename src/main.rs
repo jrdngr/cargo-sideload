19ba6d7894d06a301a3566bfb18ded87670b74e8
Jordan Grace <jordan.t.grace@gmail.com>
2021-01-18 10:45:44 -0500

    Rustfmt

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ fn main() -> anyhow::Result<()> {
     let opts = opts::Opts::parse();
     let config = Config::default()?;
 
-
     let mut downloader = download::Downloader::new(&config, &opts)?;
 
     let manifest_path = canonicalize(opts.path.join("Cargo.toml"))?;
@@ -20,7 +19,7 @@ fn main() -> anyhow::Result<()> {
 
     let lock_file_path = canonicalize(opts.path.join("Cargo.lock"))?;
     let lock_file = utils::parse_lockfile(&lock_file_path, &workspace)?;
-    
+
     let registry_index_url = utils::registry_index_url(&config, &opts.registry)?;
 
     for package_id in lock_file.iter() {
@@ -33,7 +32,6 @@ fn main() -> anyhow::Result<()> {
 
         let url = package_id.source_id().url().to_string();
         if url == registry_index_url {
-            
             let version = package_id.version().to_string();
             downloader.download(&name, &version)?;
         }
